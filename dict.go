515fb7a5ced7a85ad5e42b9502ef00b04d7990e6
Jose Luis Tallon <jltallon@adv-solutions.net>
2023-03-28 22:58:15 +0200

    Fixes & enhancements
     * Fix test which made "MergeDict" effectively a no-op
    
     * Implement "Constrained"

--- dict.go
+++ dict.go
@@ -1,13 +1,13 @@
 package algorithm
 
-// / MergeDict: merges two dictionaries;
+// MergeDict: merges two dictionaries;
 // destination values will get overwritten by those on src
 func MergeDict[T any](dst map[string]T, src map[string]T) {
 
 	if nil == src || nil == dst {
 		return
 	}
-	if &dst == &dst {
+	if &dst == &src {
 		return
 	}
 
--- misc.go
+++ misc.go
@@ -9,3 +9,14 @@ func Contains[T comparable](a []T, e T) bool {
 	}
 	return false
 }
+
+// Whether the element is within the supplied slice's values
+// Actually, it's the same as above ... only with reversed params
+func Constrain[T comparable](e T, a []T) bool {
+	for _, x := range a {
+		if e == x {
+			return true
+		}
+	}
+	return false
+}
